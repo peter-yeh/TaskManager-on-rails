1e1f81d78a1422218031598ca7c4b9278f14dbd5
Yeh <yehyuchun2@gmail.com>
2021-01-04 19:39:22 +0800

    Add react component

--- TaskManager/app/javascript/packs/application.js
+++ TaskManager/app/javascript/packs/application.js
@@ -11,3 +11,7 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true); // makes the react component avail to rails app
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
