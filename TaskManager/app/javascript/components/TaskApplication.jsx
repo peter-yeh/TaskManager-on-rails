d06c9c39d63083defaf822e83d6da5c57e9d465a
Yeh <yehyuchun2@gmail.com>
2021-01-10 16:23:44 +0800

    Add react

--- TaskManager/app/javascript/components/TaskApplication.jsx
+++ TaskManager/app/javascript/components/TaskApplication.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 
+const sampleJson = {
+  "string": "Pluralslgiht",
+  "string2": "pluralsight2"
+}
+
 class TaskApplication extends Component {
   render() {
+    const task = 'Welcome to React';
+
     return (
       <div>TaskApplication
-
-        <p>String: {this.props.name} </p>
-
-
+        <Task task={task} />
       </div>
     );
+  }
+}
+
+class Task extends Component {
+  render() {
+
+    return <h1>{this.props.task}</h1>;
   }
 }
 export default TaskApplication;
