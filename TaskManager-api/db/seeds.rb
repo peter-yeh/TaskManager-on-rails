f6c9c755911a85e4cd2dd345b68dd568051ded54
Yeh <yehyuchun2@gmail.com>
2021-01-12 19:43:00 +0800

    Update gitignore

--- TaskManager-api/db/seeds.rb
+++ TaskManager-api/db/seeds.rb
@@ -1,7 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(name:"First task", description:"body of first task", tag:"tag of first task", due:"2020-12-29 11:12:23", priority:2, done: false)
+Task.create(name:"Second task", description:"body of second task", tag:"tag of second task", due:"2021-12-29 11:12:23", priority:3, done: true)
